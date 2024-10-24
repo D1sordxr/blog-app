88c056c6d703e565e800e9d9da09bffe6e508bf8
Oleg <neevidd@yandex.ru>
2024-10-24 13:56:47 +0300

    db

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -1,3 +1,23 @@
 package postgres
 
-// TODO: init postgres
+import (
+	"database/sql"
+)
+
+type Storage struct {
+	DB       *sql.DB
+	DBConfig StorageConfig `yaml:"db_config" env-required:"true"`
+}
+
+type StorageConfig struct {
+	DBHost     string `yaml:"db_host" env-default:"localhost"`
+	DBUser     string `yaml:"db_user" env-default:"postgres"`
+	DBPassword string `yaml:"db_password" env-required:"true"`
+	DBName     string `yaml:"db_name" env-default:"postgres"`
+	DBPort     string `yaml:"db_port" env-default:"5432"`
+	DBSSLMode  string `yaml:"db_sslmode" env-default:"disabled"`
+}
+
+func Connect() {
+	// TODO: postgres connection
+}
